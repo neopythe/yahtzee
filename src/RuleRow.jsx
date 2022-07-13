b7335426798337ab076f8e70e9b956bc8ee25c86
Evan Bay <neopythe@protonmail.ch>
2022-07-13 09:36:35 -0600

    update RuleRow component with minor refactor and comments for alternate solutions

--- src/RuleRow.jsx
+++ src/RuleRow.jsx
@@ -15,15 +15,22 @@ class RuleRow extends Component {
   }
 
   render() {
+    const { score, name, doScore } = this.props
+    const disabled = score !== null
+
     return (
       <tr
         className={`flex justify-between w-full border-b border-sky-200 pt-1 ${
-          this.props.name === 'Yahtzee' && 'yahtzee'
+          name === 'Yahtzee' && 'yahtzee'
         }`}
+        // setting styles dynamically within component
+        // className={`RuleRow RuleRow-${disabled ? 'disabled' : 'active'}`}
         onClick={this.handleClick}
+        // alternative (simple) solution:
+        // onClick={disabled ? null : doScore}
       >
-        <td className="pointer-events-none">{this.props.name}</td>
-        <td className="pointer-events-none">{this.props.score}</td>
+        <td className="pointer-events-none">{name}</td>
+        <td className="pointer-events-none">{score}</td>
       </tr>
     )
   }
